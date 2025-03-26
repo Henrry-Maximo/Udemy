f85304b66c8a90bab74b3872bc6d5f77bc447850
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-03-26 09:58:33 -0300

    ✨Feat: Add types of functions

---
+++ JavaScript-TypeScript/JS-Functions/functions.js
@@ -0,0 +1,44 @@
+// declaration of function (Function Hoisting)
+function sayHello() {
+  console.log('hello');
+}
+
+// First-Class Objects
+// Function Expression
+const imAData = function() {
+  console.log('Im a data.');
+};
+
+// const imAData = function nameOfFunction() {
+//   console.log('Im a data.');
+// };
+// imAData();
+
+function executeFunction(func) {
+  console.log('Executing you function below: ')
+  func();
+};
+
+executeFunction(imAData);
+
+// Arrow Function (EcmaScript 2015 - ES6)
+const functionArrow = () => {
+  console.log('Im a arrow function');
+};
+
+functionArrow();
+
+// Within of a object
+const obj = {
+  say: function() {
+    console.log("Im saying...")
+  }
+};
+
+// const obj = {
+//   say() {
+//     console.log("Im saying...")
+//   }
+// };
+
+obj.say();

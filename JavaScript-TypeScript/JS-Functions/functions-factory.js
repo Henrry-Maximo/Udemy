b337ca472f978dce7789d1dd62713c014cf6c926
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-04-02 12:27:23 -0300

    ✨Feat: Add functions using factory

--- JavaScript-TypeScript/JS-Functions/functions-factory.js
+++ JavaScript-TypeScript/JS-Functions/functions-factory.js
@@ -13,15 +13,45 @@ function createPerson(name, surname, height, weight) {
     },
     height: height,
     peso: weight,
-    imc() {
+
+    get nameComplete() {
+      return `${this.nome} ${this.surname}`;
+      // return `${this.nome} ${this.surname}`;
+    },
+
+    // imc() {
+    //   const indice = this.peso / (this.height ** 2);
+    //   return indice.toFixed(2);
+    // }
+    // 
+
+    // becomes an attribute of my object / not function
+    // Getter
+    get imc() {
       const indice = this.peso / (this.height ** 2);
       return indice.toFixed(2);
+    },
+
+    set nameComplete(value) {
+      valor = valor.split(' ');
+      this.name = valor.shift();
+      this.surname = valor.join();
+      console.log(valor); // vetor
+      // console.log(value);
     }
   };
 }
 
 const p1 = createPerson("Henrry", "Maximo", 1.8, 80);
-console.log(p1);
-console.log(p1.say("Saying about JS."));
-const p2 = createPerson("Test", "Test");
-console.log(p2.say("Saying about JS."));
+console.log(p1.name);
+console.log(p1.surname);
+console.log(p1.imc);
+console.log(p1.name + ' ' + p1.surname);
+console.log(p1.nameComplete);
+
+p1.nameComplete('Henrry Maximo Lima');
+console.log(p1.nameComplete);
+console.log(p1.name, p1.surname);
+// console.log(p1.say("Saying about JS."));
+// const p2 = createPerson("Test", "Test");
+// console.log(p2.say("Saying about JS."));

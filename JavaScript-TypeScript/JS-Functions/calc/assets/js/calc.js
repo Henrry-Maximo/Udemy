2844cc4b3c4ee42af5bee302c9f8c9a0d7f63c78
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-04-07 10:46:58 -0300

    ✨Feat: Add methods in object for make calculation, clear display and delete element first

--- JavaScript-TypeScript/JS-Functions/calc/assets/js/calc.js
+++ JavaScript-TypeScript/JS-Functions/calc/assets/js/calc.js
@@ -5,23 +5,67 @@ function createCalc() {
     // attributes
     display: document.querySelector('.display'),
     btnClear: document.querySelector('.btn-clear'),
+    
+    // reference object key: this
+    // using method in object
+    begin() {
+      this.clickButtons();
+      this.pressEnter();
+      alert('Hello, welcome')
+    },
+
+    pressEnter() {
+      this.display.addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+          this.makeCalculation();
+        }
+      });
+    },
+
+    makeCalculation() {
+      let calculation = this.display.value;
+
+      try {
+        calculation = eval(calculation);
+        // this.display.value;
+
+        if (!calculation) {
+          alert('Conta inválida');
+          return;
+        }
+
+        this.display.value = String(calculation);
+      } catch (e) {
+        alert('Conta inválida.');
+        return;
+      }
+    },
 
     clearDisplay() {
       this.display.value = '';
     },
 
-    // using method in object
-    begin() {
-      this.clickButtons();
-      alert('Hello, welcome')
+    deleteOne() {
+      this.display.value = this.display
+      .value.slice(0, -1)
     },
+
     clickButtons() {
       document.addEventListener('click', function(e) {
         const el = e.target;
         console.log(this); // #document
 
         if (el.classList.contains('btn-num')) {
+          // Method within method
           this.btnForDisplay(el.innerText);
+        }
+
+        if (el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+
+        if (el.classList.contains('btn-del')) {
+          this.deleteOne();
         }
       }.bind(this));
       // this -> function anonymous, is necessary bind

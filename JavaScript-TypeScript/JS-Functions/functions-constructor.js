7babd72fe9692db7d3b7b1df98e2414c765f2bf7
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-04-09 16:58:48 -0300

    ✨Feat: Add functions constructors

---
+++ JavaScript-TypeScript/JS-Functions/functions-constructor.js
@@ -0,0 +1,23 @@
+
+function Person(name, surname) {
+  // Attributes or methods privates
+  const ID = 123456;
+  const methodInt = function() {
+
+  };
+
+  // Attributes or methods publics
+  this.name = name;
+  this.surname = surname;
+
+  this.method = () => {
+    console.log(this.name + ': im a method.')
+  };
+}
+
+// return object of persons
+const p1 = new Person('Henrry', 'Maximo');
+console.log(p1.name);
+console.log(p2.name);
+p1.method();
+

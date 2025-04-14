aecec09514ddde0a7726f9f8d0943a61f818b0f4
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-04-14 14:34:43 -0300

    ✨Feat: Add using getters and setters in functions contructors"

--- JavaScript-TypeScript/JS-Object-and-Protypes/getters-setters.js
+++ JavaScript-TypeScript/JS-Object-and-Protypes/getters-setters.js
@@ -16,7 +16,17 @@ function Product(name, price, stock) {
         },
 
         set: function(value) {
-            return stockPrivate = value;
+            // if (typeof valor !== 'number') {
+            //     console.log('Bad value');
+            //     return;
+            // }
+
+            if (typeof valor !== 'number') {
+                throw new TypeError('Mensagem');
+            }
+
+            // return stockPrivate = value;
+            stockPrivate = value;
             // console.log(value);
         }
     });
@@ -24,7 +34,27 @@ function Product(name, price, stock) {
 
 // getters/setters -> Simulation that method (object) of function is propertie
 
-const p1 = new Product('Shirt', 20, 3);
-console.log(p1);
-p1.stock = 'The value that i want.';
-console.log(p1.stock);
+// factory
+function createProduct() {
+    return {
+        get name() {
+            return nome;
+        },
+        set name(valor) {
+            valor = valor.replace('something', '');
+            nome = valor;
+        }
+    };
+}
+
+// const p1 = new Product('Shirt', 20, 3);
+// console.log(p1);
+// p1.stock = 'The value that i want.';
+// p1.stock = 500;
+// console.log(p1.stock);
+
+// factory function / not need of word new
+const p2 = createProduct('Camisa')
+console.log(p2); //{ nome: [Getter/Setter] }
+p2.nome = 'Any something'
+console.log(p2.nome); //{ nome: [Getter/Setter] }

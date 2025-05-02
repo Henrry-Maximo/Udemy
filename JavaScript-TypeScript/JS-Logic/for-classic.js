0ffde7ae45ef9ad55434ac5829f8fd233a0ab7ac
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-05-01 22:49:38 -0300

    ✨ Feat: Add working with for classic

---
+++ JavaScript-TypeScript/JS-Logic/for-classic.js
@@ -0,0 +1,34 @@
+// console.log('Row 0');
+// console.log('Row 1');
+// console.log('Row 2');
+// console.log('Row 3');
+// console.log('Row 4');
+// console.log('Row 5');
+
+// const limitNumberMax = 5;
+// for (let i = 0; i <= limitNumberMax; i += 2) {
+//   console.log(`Row ${i}`);
+// };
+
+// for (let i = 500; i >= 400; i -= 2) {
+//   console.log(`Row ${i}`);
+// };
+
+// for (let i = 0; i <= 100; i++) {
+//   // const par = i % 2 === 0;
+//   const par = i % 2 === 0 ? 'par' : 'ímpar';
+//   console.log(i, par);
+// };
+
+const fruits = ['Apple', 'Pear', 'Grape'];
+// console.log(fruits[2].length);
+
+for (let i = 0; i < fruits.length; i++) {
+  // template strings
+  // console.log(fruits[i]);
+  console.log(`Índice ${i}`, fruits[i]);
+};
+
+
+// for (let i = 0; i <= 5; i++) {
+// }

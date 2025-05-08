ac36b5d679ddb1b8a7f8395d7f30d71e9d836f45
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2025-05-08 15:43:38 -0300

    ✨Feat add super class of account for depositary and see balance

---
+++ JavaScript-TypeScript/JS-Object-and-Protypes/polimorfismo.js
@@ -0,0 +1,35 @@
+// Superclass
+function Account(agency, account, balance) {
+  this.agency = agency;
+  this.account = account;
+  this.balance = balance;
+}
+
+Account.prototype.withdraw = function (value) {
+  if (value > this.balance) {
+    console.log(`Saldo insuficiente, disponível: ${this.balance}`)
+    return;
+  }
+
+  this.balance -= value;
+  this.seeBalance();
+};
+
+Account.prototype.depositary = function (value) {
+  this.balance += value;
+  this.seeBalance();
+};
+
+Account.prototype.seeBalance = function () {
+  console.log(
+    `Agency/Account: ${this.agency}/${this.account} | ` +
+      `Balance: R$${this.balance.toFixed(2)}`
+  );
+};
+
+const account1 = new Account(11, 22, 10); // agência, conta, salado
+// console.log(account1); // Account { agency: 11, account: 22, balance: 10 }
+
+account1.depositary(10);
+account1.depositary(5);
+account1.withdraw(30);

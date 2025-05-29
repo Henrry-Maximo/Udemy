ec3a16449fc49737a27b934610003678ec5a6f60
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-05-29 12:39:55 -0300

    ✨ Feat: Add using promises

--- JavaScript-TypeScript/JS-Assíncrono/promises.js
+++ JavaScript-TypeScript/JS-Assíncrono/promises.js
@@ -7,9 +7,13 @@ function rand(min, max) {
 function waitThere(msg, time) {
   return new Promise((resolve, reject) => {
 
-    if (reject) {
-      return;
-    }
+    // if (reject) {
+    //   return;
+    // }
+
+    // if (typeof msg !== 'string') reject("Bad Value");
+    // if (typeof msg !== 'string') reject(new Error('ERRO'));
+    if (typeof msg !== 'string') reject(false);
 
     setTimeout(() => {
       console.log(msg);
@@ -28,19 +32,38 @@ function waitThere(msg, time) {
 // });
 
 // Not sequence
-waitThere(`Phrase 1`, rand(1, 3))
+// waitThere(`Phrase 1`, rand(1, 3))
+// .then(response => {
+//   console.log(response);
+//   return waitThere(`Phrase 2`, rand(1, 3))
+// })
+// .then(response => {
+//   console.log(response);
+//   return waitThere('phrase 3', rand(1, 3))
+// }).then(response => {
+//   console.log(response);
+// }).then(() => {
+//   console.log("I'm will be the last in display");
+// })
+// .catch();
+// waitThere(`Phrase 2`, rand(1, 3));
+// waitThere(`Phrase 3`, rand(1, 3));
+
+waitThere(`Connection with the BD`, rand(1, 3))
 .then(response => {
   console.log(response);
-  return waitThere(`Phrase 2`, rand(1, 3))
+  return waitThere(`Searching the data in database`, rand(1, 3))
 })
 .then(response => {
   console.log(response);
-  return waitThere('phrase 3', rand1(1, 3))
+  return waitThere(2222, rand(1, 3))
 }).then(response => {
   console.log(response);
 }).then(() => {
-  console.log("I'm will be the last in display");
+  console.log("Display data.");
 })
-.catch();
-// waitThere(`Phrase 2`, rand(1, 3));
-// waitThere(`Phrase 3`, rand(1, 3));
+.catch(e => {
+  console.log("Erro: ", e);
+});
+
+console.log("It's console log will be display first.")

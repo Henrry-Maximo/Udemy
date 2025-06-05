fa59b312c96f969702634b21eb00dd0c43010d57
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-06-05 16:16:21 -0300

    ✨ Feat: Add then vs async await

--- JavaScript-TypeScript/JS-Assíncrono/async-await.js
+++ JavaScript-TypeScript/JS-Assíncrono/async-await.js
@@ -1,4 +1,4 @@
-function rand(min, max) {
+function rand(min = 0, max = 3) {
   min *= 1000;
   max *= 1000;
   return Math.floor(Math.random() * (max - min) + min);
@@ -6,14 +6,53 @@ function rand(min, max) {
 
 function waitThere(msg, time) {
   return new Promise((resolve, reject) => {
-    if (typeof msg !== "string") {
-      reject('Error');
-      return;
-    }
+    setTimeout(() => {
+      if (typeof msg !== "string") {
+        reject("Error");
+        return;
+      }
 
-    resolve(msg.toUpperCase() + ' - Passei na promise');
-    return;
-  }, time);
+      resolve(msg.toUpperCase() + " - Passed in Promise");
+      return;
+    }, time);
+  });
 }
 
-waitThere('Fase 1', rand(0, 3));
+// return a promise
+// waitThere("Fase 1", rand(0, 3))
+//   .then((value) => {
+//     // code here
+//     console.log(value);
+//     return waitThere("Fase 2", rand());
+//   })
+//   .then((fase) => {
+//     console.log(fase);
+//     return waitThere('Fase 3', rand());
+//   })
+//   .then((fase) => {
+//     console.log(fase);
+//     return fase
+//   })
+//   .then(fase => {
+//     console.log('Finished in fase', fase);
+//   })
+//   .catch(e => console.log(e));
+
+// .catch((err) => {
+//    console.log(err);
+//  });
+
+async function execute() {
+  const fase1 = await waitThere('Fase 1', rand()); // aguarda a resolução da chamada
+  console.log('Finished at fase: ', fase1);
+  
+  const fase2 = await waitThere('Fase 2', rand());
+  console.log('Finished at fase: ', fase2);
+
+  const fase3 = await waitThere('Fase 3', rand());
+  console.log('Finished at fase: ', fase3);
+
+  console.log('Finished at fase: ', fase3);
+}
+
+execute();

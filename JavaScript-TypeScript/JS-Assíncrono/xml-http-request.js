d61b512de7b2034e15481c7446db42ed0878be9a
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-06-09 16:12:02 -0300

    📚 Docs: Add comments at codebase about xmlhttprequest

--- JavaScript-TypeScript/JS-Assíncrono/xml-http-request.js
+++ JavaScript-TypeScript/JS-Assíncrono/xml-http-request.js
@@ -1,18 +1,24 @@
+/*
+  A function that receive four properties:
+  - Method (GET-POST) / URL / SUCESS / ERROR 
+*/
 const request = obj => {
-  const xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest(); // Object native for do requests (old)
 
-  // Method / URL / Async or Sync ()
+  // Open request (configuration): Method / URL / Async or Sync ()
   xhr.open(obj.method, obj.url, true);
 
-  // Seeding data of user
-  xhr.send(null); // null or white
+  // Seeding data of user (POST)
+  xhr.send(null); // null or white (GET)
 
-  // xhr.onload = function () {}
+  // Waiting the response of server for fired
   xhr.addEventListener('load', () => {
     if (xhr.status >= 200 && xhr.status <= 300) {
-      obj.sucess(xhr.responseText);
+      // Call function that the user passed
+      obj.sucess(xhr.responseText); // Body how string
     } else {
       obj.error(xhr.statusText);
+
       // {
       //   code: xhr.status,
       //   msg: xhr.statusText
@@ -21,9 +27,10 @@ const request = obj => {
   });
 };
 
+// Middleware for intercept click
 document.addEventListener('click', (e) => {
-  const el = e.target;
-  const tag = el.tagName.toLowerCase();
+  const el = e.target; // this is element click
+  const tag = el.tagName.toLowerCase(); // get tag independent
 
   if (tag === "a") {
     e.preventDefault();
@@ -32,21 +39,38 @@ document.addEventListener('click', (e) => {
 })
 
 function loadPage(el) {
-  const href = el.getAttribute('href');
+  const href = el.getAttribute('href'); // Get value of link click
   console.log(href);
 
-  request({
+  const objConfig = {
     method: 'GET',
     url: href,
+
     sucess(response) {
       loadResult(response);
     },
+
     error(errorText) {
       console.error(errorText);
     }
-  });
+  };
+
+  // Think that this is your delivery man
+  request(objConfig);
+
+  // request({
+  //   method: 'GET',
+  //   url: href,
+  //   sucess(response) {
+  //     loadResult(response);
+  //   },
+  //   error(errorText) {
+  //     console.error(errorText);
+  //   }
+  // });
 }
 
+// function for update content root
 function loadResult(response) {
   const result = document.querySelector('.result');
   result.innerHTML = response;

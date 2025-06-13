bf6c707d73eb6a12a8047a007ff39ad12848b301
Henrry-Maximo <henrrylimadasilva@gmail.com>
2025-06-13 19:06:09 -0300

    ✨ Feat: Add babel for compilation

---
+++ JS-Tooling-ES6/main.js
@@ -0,0 +1,12 @@
+// const nome = 'Henrry';
+// const obj = { nome };
+// const novoObj = { ...obj };
+
+// console.log(novoObj);
+
+class Person {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+}
